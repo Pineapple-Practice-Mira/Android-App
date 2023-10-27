48983653736ea47479f193a089bd7f23cbdf7529
Evgeniy Ryzhikov <evgeniy.ryzhikov@gmail.com>
2023-10-27 14:40:55 +0700

    - подлатал Экраны

--- app/src/main/java/site/pnpl/mira/ui/greeting/fragments/GreetingFragment.kt
+++ app/src/main/java/site/pnpl/mira/ui/greeting/fragments/GreetingFragment.kt
@@ -1,17 +1,24 @@
 package site.pnpl.mira.ui.greeting.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import site.pnpl.mira.App
 import site.pnpl.mira.ui.greeting.GreetingActivity
 import site.pnpl.mira.R
+import site.pnpl.mira.data.SettingsProvider
 import site.pnpl.mira.databinding.FragmentGreetingBinding
+import site.pnpl.mira.ui.main.MainActivity
+import javax.inject.Inject
 
 class GreetingFragment : Fragment() {
     private var _binding: FragmentGreetingBinding? = null
     private val binding get() = _binding!!
+
+    @Inject lateinit var settingsProvider: SettingsProvider
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,8 +30,24 @@ class GreetingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.next.setOnClickListener {
-            (activity as GreetingActivity).navController.navigate(R.id.action_greetingFragment_to_acquaintanceFragment)
+        App.instance.appComponent.inject(this)
+
+        if (settingsProvider.getName() != "") {
+            var titleText = binding.textViewName.text.toString()
+            titleText = "$titleText,\n${settingsProvider.getName()}"
+            binding.textViewName.text = titleText
+        }
+
+        binding.greeting.setOnClickListener {
+            (requireActivity() as GreetingActivity).navController.navigate(R.id.action_greetingFragment_to_acquaintanceFragment)
+        }
+
+        binding.skip.setOnClickListener {
+            settingsProvider.firstLaunchCompleted()
+            Intent(requireActivity(), MainActivity::class.java).apply {
+                startActivity(this)
+                requireActivity().finish()
+            }
         }
     }
 
--- app/src/main/java/site/pnpl/mira/ui/greeting/fragments/InterNameFragment.kt
+++ app/src/main/java/site/pnpl/mira/ui/greeting/fragments/InterNameFragment.kt
@@ -7,11 +7,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import site.pnpl.mira.App
-import site.pnpl.mira.ui.greeting.GreetingActivity
 import site.pnpl.mira.R
 import site.pnpl.mira.data.SettingsProvider
 import site.pnpl.mira.databinding.FragmentInterNameBinding
+import site.pnpl.mira.ui.greeting.GreetingActivity
 import javax.inject.Inject
 
 class InterNameFragment : Fragment() {
@@ -33,33 +34,36 @@ class InterNameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         App.instance.appComponent.inject(this)
 
+        binding.confirm.isEnabled = false
+
         // Анимация Солнца и гор
-        sunAnim()
+        startAnimBackground()
 
-        //Врмееменая заглушка, для перехода между активити
-        binding.next.setOnClickListener {
-            (activity as GreetingActivity).navController.navigate(R.id.action_interNameFragment_to_greetingFragment)
+        var name = ""
+        binding.inputName.doAfterTextChanged { editable ->
+            name = editable.toString()
+            binding.confirm.isEnabled = name.isNotEmpty()
         }
 
-        binding.consign.setOnClickListener {
-//          Имя пока без функции обработки на ошибки и корректировки ввода символов
-            val name = binding.textField.text.toString()
-            // Введеное имя отправить и сохранить так:
-            settingsProvider.saveName(name = name)
-            // После сохранения для перехода дальше вызвать:
-            (activity as GreetingActivity).navController.navigate(R.id.action_interNameFragment_to_greetingFragment)
+        binding.confirm.setOnClickListener {
+            settingsProvider.saveName(name)
+            navigateToNextFragment()
+        }
+
+        binding.skip.setOnClickListener {
+            navigateToNextFragment()
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun navigateToNextFragment() {
+        (requireActivity() as GreetingActivity).navController.navigate(R.id.action_interNameFragment_to_greetingFragment)
     }
 
-    fun sunAnim() = with(binding) {
+
+    private fun startAnimBackground() = with(binding) {
         val sunAnim = ObjectAnimator.ofFloat(sun, View.TRANSLATION_X, 500f, 0f)
         sunAnim.duration = 1000
-        val nightAnim = ObjectAnimator.ofFloat(interName, View.ALPHA, 0.5f, 1f)
+        val nightAnim = ObjectAnimator.ofFloat(binding.root, View.ALPHA, 0.5f, 1f)
         nightAnim.duration = 500
         val mountAnim1 = ObjectAnimator.ofFloat(mountain1, View.TRANSLATION_Y, 500f, 0f)
         mountAnim1.duration = 1000
@@ -72,5 +76,10 @@ class InterNameFragment : Fragment() {
         animatorSun.playTogether(sunAnim, nightAnim, mountAnim3, mountAnim2, mountAnim1)
         animatorSun.start()
 
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
