c8fe2a437bf518115149f5b79d2d2bdf701a7179
evgen <evgeniy.ryzhikov@gmail.com>
2024-02-08 22:39:51 +0700

    - 0.6.17 - not send analytics event in debug build

--- app/src/main/java/site/pnpl/mira/domain/analitycs/MixPanelAnalytics.kt
+++ app/src/main/java/site/pnpl/mira/domain/analitycs/MixPanelAnalytics.kt
@@ -1,8 +1,10 @@
 package site.pnpl.mira.domain.analitycs
 
 import android.content.Context
+import android.os.Build
 import com.mixpanel.android.mpmetrics.MixpanelAPI
 import org.json.JSONObject
+import site.pnpl.mira.BuildConfig
 import javax.inject.Inject
 
 
@@ -11,16 +13,18 @@ class MixPanelAnalytics @Inject constructor(
 ) : Analytics {
 
     private val mixpanelAPI: MixpanelAPI =
-        MixpanelAPI.getInstance(applicationContext, MixPanel.TOKEN, true)
+        MixpanelAPI.getInstance(applicationContext, MixPanel.TOKEN, !BuildConfig.DEBUG)
 
     override fun sendEvent(@AnalyticsEvent.Name eventName: String, params: List<EventParameter>) {
-        val props = JSONObject()
-        if (params.isNotEmpty()) {
-            params.forEach { parameter ->
-                props.put(parameter.key, parameter.value)
+        if (!BuildConfig.DEBUG) {
+            val props = JSONObject()
+            if (params.isNotEmpty()) {
+                params.forEach { parameter ->
+                    props.put(parameter.key, parameter.value)
+                }
             }
+            mixpanelAPI.track(eventName, props)
         }
-        mixpanelAPI.track(eventName, props)
     }
 
 }
