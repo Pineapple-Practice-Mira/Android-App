58cab30f13bdf1f4c4627e01e20e44ff6c09185c
Ryzhikoff <72570184+Ryzhikoff@users.noreply.github.com>
2024-02-08 22:43:26 +0700

    Delete app/src/ip/java/site/pnpl/mira/data/remote/ApiConstants.kt

--- app/src/ip/java/site/pnpl/mira/data/remote/ApiConstants.kt
+++
@@ -1,6 +0,0 @@
-package site.pnpl.mira.data.remote
-
-object ApiConstants {
-//    const val BASE_URL = "https://backend-kappa-teal.vercel.app/"
-    const val BASE_URL = "http://185.154.195.58/"
-}
